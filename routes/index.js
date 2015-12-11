f049b262dedd48cbff6211e639972f9f68838cd4
macwram <macr@sourcepad.com>
2015-12-12 00:50:58 +0800

    work page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+router.get('/works', function(req, res, next) {
+  res.render('works/index', { title: 'Works' });
+});
+router.get('/works/:work', function(req, res, next) {
+	var work = req.params.work.charAt(0).toUpperCase() + req.params.work.slice(1);
+  res.render('works/show', { title: work });
+});
 
 module.exports = router;
